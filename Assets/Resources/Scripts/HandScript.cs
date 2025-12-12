31482a82256cf6fed244540824dd443523d34419
Nio8L <nio8.minecraft@gmail.com>
2025-12-12 23:36:59 +0200

    Hand grid movement

--- Assets/Resources/Scripts/HandScript.cs
+++ Assets/Resources/Scripts/HandScript.cs
@@ -1,33 +1,47 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class HandScript : MonoBehaviour
 {
     public float speed = 5f;
+
+    int x = 0, y = 0;
+
+    private void Start() {
+        transform.position = LevelEditor.instance.transform.position;
+    }
+
     void Update()
     {
-        
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
+            Move(x, y + 1);
             transform.rotation = Quaternion.Euler(0, 0, 90);
-            transform.position = new Vector2(transform.position.x, transform.position.y + speed);
 
         }
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
+            Move(x, y - 1);
             transform.rotation = Quaternion.Euler(0, 0, 270);
-            transform.position = new Vector2(transform.position.x, transform.position.y - speed);
         }
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            Move(x - 1, y);
             transform.rotation = Quaternion.Euler(0, 0, 180);
-            transform.position = new Vector2(transform.position.x - speed, transform.position.y);
         }
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
+            Move(x + 1, y);
             transform.rotation = Quaternion.Euler(0, 0, 0);
-            transform.position = new Vector2(transform.position.x + speed, transform.position.y);
         }
+    }
+
+    void Move(int x, int y)
+    {
+        transform.position = LevelEditor.instance.tiles[x, y].transform.position;
+        this.x = x;
+        this.y = y;
     }
 }
