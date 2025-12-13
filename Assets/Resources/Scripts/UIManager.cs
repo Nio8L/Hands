d0dc64761a101f63d69acf674d110403af8a9ca0
aycileko2024 <45941471+aycileko@users.noreply.github.com>
2025-12-13 02:50:10 +0100

    Start Menu has been pretty much implemented

--- Assets/Resources/Scripts/UIManager.cs
+++ Assets/Resources/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
  using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 using System.Collections;
 
 public class UIManager : MonoBehaviour
@@ -7,5 +8,28 @@ public class UIManager : MonoBehaviour
     public void OnStartButtonPress()
     {
         SceneManager.LoadScene("Level1");
+    }
+
+    public void OnOptionsButtonPress()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void OnExitButtonPress()
+    {
+        Application.Quit();
+    }
+
+    public void OnCreditsButtonPress()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void pauseMenu()
+    {
+        if (Input.GetKeyDown(Key.Escape))
+        {
+            Time.timeScale = 0f;
+            SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+        }
     }
 }
