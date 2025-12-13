56926133c89210f993faab986e54c9fa2e11d976
aycileko2024 <45941471+aycileko@users.noreply.github.com>
2025-12-13 21:31:21 +0100

    Revert "Merge branch 'main' of https://github.com/Nio8L/Hands"
    This reverts commit 04d88f3b0b611ff526e67e69d0954d11ba8414d0, reversing
    changes made to e5a835537c85955a2682ba975b5202164c8c9b3a.

--- Assets/Resources/Scripts/Tile/Tile.cs
+++ Assets/Resources/Scripts/Tile/Tile.cs
@@ -21,7 +21,6 @@ public class Tile : MonoBehaviour
 
     private void Start() {
         defaultType = Instantiate(type);
-        type = Instantiate(type);
     }
 
     public void ChangeColor()
@@ -41,7 +40,7 @@ public class Tile : MonoBehaviour
     {
         if(tileType == null) return;
 
-        type = Instantiate(tileType);
+        type = tileType;
         ChangeColor();
     }
 
