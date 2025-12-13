3299aacf35f39ecaf67b2d06d697f80c0a655a0f
RaToni36 <ratonimarvel@gmail.com>
2025-12-14 00:24:02 +0100

    Pause Menu
    Made Pause Menu prefab

--- Assets/Resources/Scripts/UIManager.cs
+++ Assets/Resources/Scripts/UIManager.cs
@@ -5,6 +5,15 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour
 {
+    public CanvasGroup pause;
+    bool isActive = false;
+    public Canvas pauseMenu;
+
+    private void Start()
+    {
+        pause.alpha = 0;
+        pauseMenu.GetComponent<Canvas>().enabled = false;
+    }
     public void OnStartButtonPress()
     {
         SceneManager.LoadScene("Level1");
@@ -19,24 +28,42 @@ public class UIManager : MonoBehaviour
         Application.Quit();
     }
 
+    public void OnMenuButtonPress()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
     public void OnCreditsButtonPress()
     {
         SceneManager.LoadScene("Credits");
     }
 
-    public void pauseMenu()
+    public void OnResumeButtonPress()
+    {
+        Time.timeScale = 1f;
+        pause.alpha = 0;
+        isActive = false;
+    }
+
+    public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Time.timeScale = 0f;
-            SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
-        }
-    }
+            if (isActive == false)
+            {
+                pauseMenu.GetComponent<Canvas>().enabled = true;
+                Time.timeScale = 0f;
+                pause.alpha = 1;
+                isActive = true;
+            }
+            else
+            {
+                pauseMenu.GetComponent<Canvas>().enabled = false;
+                Time.timeScale = 1f;
+                pause.alpha = 0;
+                isActive = false;
 
-    public void OnResumeButtonPress()
-    {
-        //TO DO:
-        //Hide Pause menu using
-        //SetActive(false);
+            }
+        }
     }
 }
