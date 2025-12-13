d717552cf6b7f4323326e0f7beb59635b222ba8c
Nio8L <nio8.minecraft@gmail.com>
2025-12-13 22:01:32 +0200

    Doors
    Level 5 doors working

---
+++ Assets/Resources/Scripts/Tile/Tile Types/ButtonType.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TileType/Button")]
+public class ButtonType : TileType
+{
+    public int targetX, targetY;
+    public TileType openDoor;
+
+    public override void Effect(HandScript hand)
+    {
+        Level.instance.tiles[targetX, targetY].SetType(openDoor);
+    }
+
+    public override void Uneffect(HandScript hand)
+    {
+        Level.instance.tiles[targetX, targetY].Default();
+    }
+}
