e5a835537c85955a2682ba975b5202164c8c9b3a
aycileko2024 <45941471+aycileko@users.noreply.github.com>
2025-12-13 21:29:56 +0100

    Implemented move counter for both hands

--- Assets/Resources/Scripts/HandScript.cs
+++ Assets/Resources/Scripts/HandScript.cs
@@ -1,10 +1,7 @@
-using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class HandScript : MonoBehaviour
 {
@@ -13,15 +10,18 @@ public class HandScript : MonoBehaviour
     public int x = 0, y = 0;
 
     public List<Tile> handSegment;
-    //public TileType handPart;
     public int maxLength;
     int controller;
     Color handColor;
+    public int movesLeft;
 
     public void Setup(Color color, int controller)
     {
         handColor = color;
         this.controller = controller;
+        maxLength = 3;
+        movesLeft = maxLength;
+        Debug.Log($"[HAND {controller}] Moves left: {movesLeft}");
     }
 
     void Update()
@@ -48,7 +48,9 @@ public class HandScript : MonoBehaviour
                 Move(x + 1, y);
                 transform.rotation = Quaternion.Euler(0, 0, 0);
             }
-        }else{
+        }
+        else
+        {
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 Move(x, y + 1);
@@ -70,7 +72,6 @@ public class HandScript : MonoBehaviour
                 transform.rotation = Quaternion.Euler(0, 0, 0);
             }
         }
-        
     }
 
     void Move(int x, int y)
@@ -79,32 +80,37 @@ public class HandScript : MonoBehaviour
         {
             Tile targetTile = Level.instance.tiles[x, y];
             Tile originTile = Level.instance.tiles[this.x, this.y];
-            
+
+            // RETRACT
             if (handSegment.Contains(targetTile))
             {
                 if (handSegment[^1].x == targetTile.x && handSegment[^1].y == targetTile.y)
                 {
                     if (handSegment.Count > maxLength)
                     {
-                        Retract();
-                        return;
+                     Retract();
+                        return;   
                     }
-
                     handSegment.Remove(targetTile);
-                    targetTile.Default();
+                    if (handSegment.Count < maxLength) movesLeft += 1;
+                    Debug.Log($"[HAND {controller}] Moves left: {movesLeft}");
 
+                    targetTile.Default();
                     UpdatePosition(x, y);
-                    
-                    //targetTile.Activate(this);
                     originTile.Deactivate(this);
                 }
-            }else if (targetTile.type.walkable && handSegment.Count < maxLength)
+            }
+            // EXTEND
+            else if (targetTile.type.walkable && movesLeft > 0)
             {
                 handSegment.Add(originTile);
+                movesLeft -= 1;
+                Debug.Log($"[HAND {controller}] Moves left: {movesLeft}");
+
                 originTile.HandOn(handColor);
 
                 UpdatePosition(x, y);
-                
+
                 targetTile.Activate(this);
                 originTile.Deactivate(this);
             }
@@ -114,48 +120,38 @@ public class HandScript : MonoBehaviour
     public void ForceMove(int direction, int length)
     {
         int dx = 0;
-        int dy = 0; 
-     
+        int dy = 0;
+
         switch (direction)
         {
-            case 0:
-                dy = 1;
-                break;
-            case 1:
-                dx = 1;
-                break;
-            case 2:
-                dy = -1;
-                break;
-            case 3:
-                dx = -1;
-                break; 
-            default:
-                dy = 0;
-                break;
+            case 0: dy = 1; break;
+            case 1: dx = 1; break;
+            case 2: dy = -1; break;
+            case 3: dx = -1; break;
         }
 
         for (int i = 0; i < length; i++)
         {
+
             Tile targetTile = Level.instance.tiles[x + dx, y + dy];
             Tile originTile = Level.instance.tiles[x, y];
-            
+
             if (targetTile.type.walkable)
             {
                 handSegment.Add(originTile);
+                Debug.Log($"[HAND {controller}] Moves left: {movesLeft}");
+
                 originTile.HandOn(handColor);
 
                 UpdatePosition(x + dx, y + dy);
-                
+
                 targetTile.Activate(this);
-                //originTile.Deactivate(this);
             }
             else
             {
                 return;
             }
         }
-
     }
 
     public void Retract()
@@ -164,12 +160,12 @@ public class HandScript : MonoBehaviour
         {
             Tile targetTile = handSegment[^1];
             Tile originTile = Level.instance.tiles[x, y];
+            movesLeft = maxLength;
             handSegment.Remove(targetTile);
-            targetTile.Default();
+            Debug.Log($"[HAND {controller}] Moves left: {movesLeft}");
 
+            targetTile.Default();
             UpdatePosition(targetTile.x, targetTile.y);
-                    
-            //targetTile.Activate(this);
             originTile.Deactivate(this);
         }
     }
